05086a89ada4f9f91548177e70f9d109be16bc2e
yenlee38 <ngocyen174308@gmail.com>
2021-08-18 14:01:35 +0700

    change name

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import Signup from './src/screens/signup';
-import ForgotPass from './src/screens/forgot-pass';
-import Profile from './src/screens/profile';
-import OTPAuth from './src/screens/otp-auth';
+import Signup from './src/screens/signup.screen';
+import ForgotPass from './src/screens/forgot-pass.screen';
+import Profile from './src/screens/profile.screen';
+import OTPAuth from './src/screens/otp-auth.screen';
 const Project = createStackNavigator(
   {
     Signup: {
