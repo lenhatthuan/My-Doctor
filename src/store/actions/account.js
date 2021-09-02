e2f22742e748892421186d30d8436caa55a78508
yenlee38 <ngocyen174308@gmail.com>
2021-09-02 20:54:38 +0700

    signin

--- src/store/actions/account.js
+++ src/store/actions/account.js
@@ -1,26 +1,28 @@
-const BASE_URL = process.env.BASE_URL;
+import { environment } from "../../../environment/enviroment";
 
-const signin = (username, pass) =>{
+const BASE_URL = environment.baseURL;
+const header = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+export const signin = async (username, pass) =>{
     return(
-        fetch(BASE_URL  + '/accounts', {
+        await fetch(BASE_URL  + '/accounts/signin', {
             method: 'POST',
-            headers:{
-                Accept: 'application/json',
-             'Content-Type': 'application/json'
-            },
+            headers: header
+           ,
             body: JSON.stringify({
                 username: username,
-                password: pass
+                password: pass 
             })
         })
         .then((response) => response.json())
         .then((json) => {
+            console.log("account: " + json.token + " username: " + username)
           return json;
         })
-        .catch((error) => {
-          console.error("sigin fail" + error);
+        .catch((error) => { 
+          console.error("sigin fail *********************: " + error);
         })
     )
 }
-
-module.exports = {signin}
