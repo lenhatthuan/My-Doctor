2fa03a263109f992a1e3bdca3d1abdbc2da71463
THU-AN <lenhatthuan2000@gmail.com>
2021-09-19 22:48:40 +0700

    Revert "Revert "commit""
    This reverts commit 579ca611397f21f869c02f83c29c8c3c123e96dc.

--- src/screens/OTPScreen.js
+++ src/screens/OTPScreen.js
@@ -1,9 +1,44 @@
-import React, { useState, useEffect  } from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-import OTPInputView from 'react-native-otp-input';
-// import auth from '@react-native-firebase/auth';
-const OTPScreen = (props) =>{
-  return (<View><Text>OTP</Text></View>)
+import * as React from "react";
+import { View, SafeAreaView, Text, Button } from "react-native";
+import OTPInputView from "react-native-otp-input";
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
+import { FIREBASE_CONFIG } from "../../environment/enviroment";
+import { styles } from "../theme/style";
+
+export default function OTPScreen({ authOTP, sendOTP }) {
+  const recaptchaVerifier = React.useRef();
+  const [verificationCode, setVerificationCode] = React.useState();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaVerifier}
+        firebaseConfig={FIREBASE_CONFIG}
+        //attemptInvisibleVerification={true}
+      />
+      <Text style={styles.title}>Xác thực bằng mã OTP</Text>
+      <OTPInputView
+        style={{ height: 100 }}
+        pinCount={6}
+        code={verificationCode}
+        onCodeFilled={(text) => setVerificationCode(text)}
+        codeInputFieldStyle={styles.underlineStyleBase}
+        codeInputHighlightStyle={styles.underlineStyleHighLighted}
+      />
+      <View style={styles.space}>
+        <Button
+          title="Xác thực"
+          onPress={() => {
+            authOTP(verificationCode);
+          }}
+        />
+        <Button
+          title="Gửi lại mã"
+          onPress={() => {
+            sendOTP();
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
-      
-export default OTPScreen;
