8502ee3ac5d6ac3102fa31e53953b2eab9d95687
unknown <lenhatthuan2000@gmail.com>
2022-06-17 23:34:10 +0700

    get active room by dept

--- src/store/actions/room.js
+++ src/store/actions/room.js
@@ -4,7 +4,7 @@ const BASE_URL = environment.baseURL;
 
 export const getRoomByDepartment = async (deparment) => {
   try {
-    const response = await fetch(BASE_URL + `/room/${deparment}`);
+    const response = await fetch(BASE_URL + `/roomActive/${deparment}`);
     const json = await response.json();
     return json.room;
   } catch (err) {
